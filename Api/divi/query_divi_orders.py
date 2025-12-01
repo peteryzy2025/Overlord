445a0a1218b735fb93619f403101f6ddc1aa49d3
东方月初 <yeyan1573@163.com>
2025-12-01 09:28:29 +0800

    12-01 - 1 自动更新数据优化，以及通知邓优化

--- Api/divi/query_divi_orders.py
+++ Api/divi/query_divi_orders.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
-
 import os
 import sys
 import django
@@ -15,36 +13,49 @@ django.setup()
 
 from divi_order_service import query_divi_order, send_wechat_with_retry
 
-# ================== 通用 Divi 请求封装（就是你原来的签名逻辑） ==================
+from datetime import datetime, timedelta
+from typing import Tuple, Optional
+def get_previous_hour_range(current_time: Optional[datetime] = None) -> Tuple[str, str]:
+    """
+    获取前一个小时的时间范围
 
-PARTNER_CODE = "7607f3480484b48235"
-SECRET = "655d0d4e9534f0e37381"
-BASE_URL = "http://www.dividiy.com/partner/use/service"
+    :param current_time: 当前时间，默认为系统当前时间
+    :return: (开始时间, 结束时间) 字符串元组，格式为 YYYY-MM-DD HH:MM:SS
+    """
+    # 如果没有传入时间，则获取当前系统时间
+    if current_time is None:
+        current_time = datetime.now()
 
-# 企业微信Webhook URL列表
-WECHAT_WEBHOOK_URLS = [
-    "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=d910f7a2-bb75-435e-a8ce-777efdab8eab",
-    "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=4391c008-ea12-4243-a0aa-72f12b31cb21",
-    "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=a4bbf1b1-fb28-42a0-a291-bcf207180d4c",
-    "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=f5482538-c667-4525-bab6-ae8334d03548",
-    "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=874e31e7-d6d5-4016-b6b3-478ed60a2fc0",
-]
+    # 减去1小时，得到前一个小时的时间点
+    prev_hour = current_time - timedelta(hours=1)
+
+    # 获取前一个小时开始时间（00分00秒）
+    start_time = prev_hour.replace(minute=0, second=0, microsecond=0)
+
+    # 获取前一个小时结束时间（59分59秒）
+    end_time = prev_hour.replace(minute=59, second=59, microsecond=0)
+
+    # 格式化为字符串
+    return (
+        start_time.strftime("%Y-%m-%d %H:%M:%S"),
+        end_time.strftime("%Y-%m-%d %H:%M:%S")
+    )
+s,e = get_previous_hour_range()
+print(s,e)
 exists, orders, resp_json = query_divi_order(amazon_order_id=None, brand_id=None, days=None,
-                                             start_time="2025-12-01 01:00:00", end_time="2025-12-01 08:59:59")
+                                             start_time=s, end_time=e)
 mess_list = []
 for order in orders:
     meet = False
     if len(order.get("orderGoodsList")) > 1:
         meet = True
-    order_goods_list = []
     for order_goods in order.get("orderGoodsList"):
         product_name = order_goods.get("productName")  # 产品名字
         quantity_ordered = order_goods.get("quantityOrdered")  # 订购数量
-        order_goods_list.append((product_name, quantity_ordered))
         if quantity_ordered > 1:
             meet = True
     if meet:
-        mess_list.append((order.get("amazonOrderId"), order_goods_list))
-mess_str = "\n".join([str(item) for item in mess_list])
+        mess_list.append((order.get("amazonOrderId")))
+mess_str = " ".join(list(mess_list))
 send_wechat_with_retry(mess_str)
-print(mess_list)
+print(mess_str)
