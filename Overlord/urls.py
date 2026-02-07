077b6e1c82aca3974a0d9d99f777aaae13d1b523
东方月初 <yeyan1573@163.com>
2026-02-07 15:59:40 +0800

    1.2.11 新增个人中心+头像

--- Overlord/urls.py
+++ Overlord/urls.py
@@ -2,6 +2,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -17,3 +18,7 @@ urlpatterns = [
     path('inventory/', include('inventory.urls')),
     path('data_req/', include('data_req.urls')),
 ]
+
+# 开发环境下提供媒体文件服务
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
