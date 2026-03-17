17e1040d152a2a4cc09e69b60c737f7b08ab2dc0
杨振宇 <peteryzy@outlook.com>
2026-03-17 09:23:21 +0800

    优化页面加载方式

--- aba/urls.py
+++ aba/urls.py
@@ -9,5 +9,6 @@ urlpatterns = [
     
     # API 接口
     path('api/data/', views.get_aba_data_api, name='api_aba_data'),
+    path('api/update-denoising/', views.update_search_term_denoising_api, name='api_update_search_term_denoising'),
     path('api/available-weeks/', views.get_available_weeks_api, name='api_aba_available_weeks'),
 ]
