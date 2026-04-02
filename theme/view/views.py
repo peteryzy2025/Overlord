5fe06e62309c5fa7af4d35ba0fede2ffee6fa54e
杨振宇 <peteryzy@outlook.com>
2026-04-02 15:53:44 +0800

    修侧边栏bug

--- theme/view/views.py
+++ theme/view/views.py
@@ -102,6 +102,7 @@ def product_list_page(request):
     context = {
         'page_title': 'Amazon新奇特',
         'active_nav': 'theme_products',
+        'active_page':'theme_product_list',
         'stats': stats,
         'latest_deal_stats': latest_deal_stats,
         'date_range': date_range,
